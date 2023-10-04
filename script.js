9ccd62001e41c72b8beed8f7777224aed8ede6f5
Kiwiiee <114266029+Kiwiiee@users.noreply.github.com>
2023-10-04 00:51:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,44 @@ function verificar(){
     let cep = document.getElementById("cep").value
     let nome = document.getElementById("nome").value
     let numero = document.getElementById("numero_en").value
+    let campo = [].slice.call(document.querySelectorAll('.campo'));
+    let campo2 = []
+    campo.forEach((camp) => {
+        campo2.push(camp.value)
+
+    })
+
+    if(campo2[6] != ''){
+        campo2[9] = campo2[5]
+        campo2[10] = campo2[6]
+        campo2[11] = campo2[7]
+        campo2[12] = campo2[8]
+
+    }else{
+        campo2[5] = campo2[9]
+        campo2[6] = campo2[10]
+        campo2[7] = campo2[11]
+        campo2[8] = campo2[12]
+
+    }
+
+    
+    let retornar = true 
+
+    campo2.forEach((camp) => {
+        if(camp == ''){
+            retornar = false
+
+        }
+
+    })
+
+    if(retornar == false){
+        alert("Por favor preencha todos os campos")
+        return false
+
+    }
+
     let metodo = [].slice.call(document.querySelectorAll('.radio'));
     let metodo2 = []
     metodo.forEach((met) => {
@@ -31,11 +69,84 @@ function verificar(){
         }
 
     })
+    
 
     if(metodos === false){
         alert("Por favor escolha uma forma de pagamento")
-        // return false
+        return false
 
+    }
+
+    let check = [].slice.call(document.querySelectorAll('.check'));
+    let check2 = []
+    check.forEach((checkbox) => {
+        check2.push(checkbox.checked)
+
+    })
+
+    let checados = false 
+
+    check2.forEach((checkbox) => {
+        if(checkbox === true){
+            checados = true
+        }
+
+    })
+
+    if(checados === false){
+        alert("Por favor escolha um ou mais itens")
+        return false
+
+    }
+
+
+    if(telefone.length != 11){
+        alert("Por favor insira um número valido com o DDD")
+        return false
+
+    }
+
+    if(isNaN(numero) || isNaN(cep) || isNaN(campo2[7]) || isNaN(campo2[6]) || isNaN(telefone)){
+        alert("Os campo de CEP, CVV, Número de Cartão, Telefone e Número da casa devem ser preenchidos apenas com números")
+        return false
+
+    }
+
+    if(campo2[7].length != 3){
+        alert("Por favor insira um CVV válido")
+        return false
+
+    }
+
+    if(cep.length != 8){
+        alert("Por favor insira um CEP valido")
+        return false
+
+    }
+
+    if(campo2[6].length != 16){
+        alert("Por favor insira um número de cartão válido")
+        return false
+
+    }
+
+    let primeira_letra = Array.from(nome)[0]
+    let primeira_letra_up = primeira_letra.toUpperCase()
+    let nome_completo = nome.split(' ')
+
+    if(nome_completo[1] == undefined){
+        alert("Por favor insira seu nome completo")
+        return false
+    }
+
+    if(primeira_letra != primeira_letra_up){
+        alert("Por favor insira seu nome com a letra maiúscula")
+        return false
+
+    }
+
+    return true
+}
     }
 
     let check = [].slice.call(document.querySelectorAll('.check'));
